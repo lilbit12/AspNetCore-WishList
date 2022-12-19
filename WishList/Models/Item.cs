912fb0b89c5e8efd680e33f1556ab35c513f813b
Radoslaw Kwiatkowski <Radoslaw_Kwiatkowski@next.co.uk>
2022-12-19 15:42:16 +0000

    v1

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -11,7 +11,7 @@ namespace WishList.Models
         public int Id { get; set; }
 
         [Required]
-        [StringLength(50)]
+        [MaxLength(50)]
         public string Description { get; set; }
 
     }
